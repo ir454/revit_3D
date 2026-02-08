d831a64faa6c23714bedcdb0e115e6a4559b6dcf
ir454 <ir454@nyu.edu>
2026-02-07 20:23:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ document.getElementById("searchBtn").onclick = async () => {
   const artist = document.getElementById("artist").value;
   const song = document.getElementById("song").value;
   const lyrics = document.getElementById("lyrics");
-  const ytLink = document.getElementById("ytLink");
 
   if (!artist || !song) return;
 
@@ -10,8 +9,5 @@ document.getElementById("searchBtn").onclick = async () => {
   const data = await res.json();
 
   lyrics.textContent = data.lyrics || "Lyrics not found";
-
-  const query = encodeURIComponent(`${artist} ${song}`);
-  ytLink.textContent = "Open YouTube video";
-  ytLink.href = `https://www.youtube.com/results?search_query=${query}`;
 };
+
