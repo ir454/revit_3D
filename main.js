d7045ac8416c258c560acd404e7f3e77ef484ce8
ir454 <ir454@nyu.edu>
2026-02-07 20:36:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,9 @@ document.getElementById("searchBtn").onclick = async () => {
   const song = document.getElementById("song").value;
   const lyrics = document.getElementById("lyrics");
 
-  if (!artist || !song) {
-    lyrics.textContent = "Please enter both artist and song name.";
-    return;
-  }
+  if (!artist || !song) return;
 
   const res = await fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
   const data = await res.json();
-
   lyrics.textContent = data.lyrics || "Lyrics not found";
 };
